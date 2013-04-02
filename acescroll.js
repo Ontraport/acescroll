4804281723899f2a5cd4d1d806ff34cbead6b61c
Jesse Baird <jebaird@gmail.com>
2013-04-02 16:59:47 -0500

    added helpers for vert and hoz scroll bars

--- acescroll.js
+++ acescroll.js
@@ -11,6 +11,40 @@ steal(
 	
 	'./lib/jquery-mousewheel-3.0.5/jquery.mousewheel.js',
 	'./lib/jquery.jb.acescroll.js',
+	function(){
+	/*
+	 * setup the horz and vert plugins
+	 */
 	
-
+	/*
+	 * short cut for vertical 
+	 */
+	$.fn.acescrollv = function( position ){
+		return this.acescroll({
+			position: $.extend( {}, {
+				my: 'left top',
+				at: 'right top',
+				offset: '11 0',
+				collision: 'none'
+			}, position )
+		});
+	};
+	
+	/*
+	 * short cut for vertical horizontal
+	 */
+	$.fn.acescrollh = function( position ){
+		return this.acescroll({
+			orientation: 'horizontal',
+			position: $.extend( {}, {
+				my:'left bottom',
+                at:'left bottom',
+                offset:'0 20',
+                collision:'none'
+			}, position )
+		});
+	};
+	
+	
+	}
 )
