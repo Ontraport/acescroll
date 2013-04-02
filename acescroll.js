53db3e25489ba9f2fe692e62cb1112a8e1a75e35
Jesse Baird <jebaird@gmail.com>
2013-04-02 15:56:19 -0500

    added theme css

--- acescroll.js
+++ acescroll.js
@@ -1,9 +1,16 @@
+/*
+ * assumses that you have jquery-ui checked out to jqueryui
+ */
 steal(
 	'jquery',
-	'jqueryui'
+	'jqueryui',
+	'./lib/jquery.jb.acescroll.css'
 )
 .then(
+	'./acescroll.css',
+	
 	'./lib/jquery-mousewheel-3.0.5/jquery.mousewheel.js',
 	'./lib/jquery.jb.acescroll.js',
-	'./lib/jquery.jb.acescroll.css'
+	
+
 )
